700e4e08f74fada36ff669293b55ddda809b38b5
KimJoonHeeC <KimJoonHeeC@gmail.com>
2017-07-12 19:31:21 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ from tasks import *
 import os
 
 bot = MsBot(port=int(os.environ['PORT']))
-#bot.add_process(echo_response)
-bot.add_process(djn_response)
+bot.add_process(echo_response)
+#bot.add_process(djn_response)
 bot.run()
