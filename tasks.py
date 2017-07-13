e8406f072d6ec6a808388381f8df63d62803f295
KimJoonHeeC <KimJoonHeeC@gmail.com>
2017-07-13 17:31:51 +0900

    Add files via upload

--- tasks.py
+++ tasks.py
@@ -3,10 +3,10 @@ from microsoftbotframework import ReplyToActivity
 djn_reply = "ㅇㅇ"
 
 def echo_response(message):
-  if message["type"] == "message":
-    ReplyToActivity(fill=message,
-                    text=message["text"]).send()
+    if message["type"] == "message":
+        ReplyToActivity(fill=message,
+                        text=message["text"]).send()
 
 def djn_response(message):
     if message["type"] == "message":
-        ReplyToActivity(fill=djn_reply, text=djn_reply).send()
+        ReplyToActivity(fill=djn_reply, text=message["text"]).send()
