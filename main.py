115d40cffa7d6495aba973528408b92abb3b7a5b
KimJoonHeeC <KimJoonHeeC@gmail.com>
2017-07-16 00:48:04 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -5,7 +5,5 @@ from tasks import *
 import os
 
 bot = MsBot(port=int(os.environ['PORT']))
-#bot.add_process(echo_response)
-#bot.add_process(djn_response)
-bot.add_process(djn_echo)
+bot.add_process(echo_response)
 bot.run()
--- tasks.py
+++ tasks.py
@@ -1,17 +1,44 @@
 from microsoftbotframework import ReplyToActivity
+import requests
+import json
+from datetime import datetime
 
-djn_reply = "ㅇㅇ"
+# https://api.korbit.co.kr/v1/ticker
 
 def echo_response(message):
+    print(message)
+  
     if message["type"] == "message":
-        ReplyToActivity(fill=message,
-                        text=message["text"]).send()
+        if "bitcoin" in message["text"] or "비트코인" in message["text"]:
+            r = requests.get("https://api.korbit.co.kr/v1/ticker")
+            bitcoin_price = r.json()["last"]
+            current_time = datetime.fromtimestamp(r.json()['timestamp']/1000).strftime('%Y-%m-%d %H:%M:%S')
+            msg = "bitcoin price is %s at %s" % (bitcoin_price, current_time)
+            print(msg)
+            ReplyToActivity(fill=message, text=msg).send()
+            
+        elif "ㅇㅇ" in message["text"]:
+            djn_reply = "그래그래 ㅇㅇ"
+            ReplyToActivity(fill=message, text=djn_reply).send()
+        
+        else:
+            data =  {
+                "documents": [{ "language": "en", "id": "1", "text": message["text"]}]
+            }
+            headers = {'Ocp-Apim-Subscription-Key': '4cfe6f744f1b486db3fa83d874bafdd9', 
+                       'Content-Type': 'application/json',
+                       'Accept': 'application/json',
+                      }
 
-def djn_response(message):
-    if message["type"] == "message":
-        ReplyToActivity(fill=message, text=djn_reply).send()
-        
-def djn_echo(message):
-    if message["type"] == "message":
-        ReplyToActivity(fill=message, text=message["text"]+djn_reply+"...").send()
-        
+            r = requests.post("https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment",
+                              data=json.dumps(data),headers=headers)
+            emo_score = r.json()["documents"][0]["score"]
+            msg = "emotion score is %s\n" % emo_score
+
+            if emo_score > 0.5:
+                msg = msg + "You look happy!"
+            else:
+                msg = msg + "You look unhappy.."
+
+            print(msg)
+            ReplyToActivity(fill=message, text=msg).send()
